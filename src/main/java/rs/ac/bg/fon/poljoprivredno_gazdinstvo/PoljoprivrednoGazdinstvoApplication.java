f2432ddfb0b7ee0247055977119c824d3fbe18ad
ddjole99 <djole.3453@gmail.com>
2025-11-25 19:41:25 +0100

    Kreiranje projekta i dodavanje pocetnih dependency-ja
    -Spring web
    -Spring Data JPA
    -Validation
    -H2 Database
    -Lombok

---
+++ src/main/java/rs/ac/bg/fon/poljoprivredno_gazdinstvo/PoljoprivrednoGazdinstvoApplication.java
@@ -0,0 +1,12 @@
+package rs.ac.bg.fon.poljoprivredno_gazdinstvo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoljoprivrednoGazdinstvoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoljoprivrednoGazdinstvoApplication.class, args);
+	}
+}
